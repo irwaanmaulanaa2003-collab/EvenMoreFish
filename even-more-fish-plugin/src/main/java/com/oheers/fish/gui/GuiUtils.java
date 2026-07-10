e3a285a7796ab178e12dc0b04d707e4be3454957
Irwan Maulana <irwaanmaulanaa2003@gmail.com>
2026-07-11 03:04:14 +0700

    Fix rod shop FishUtils import

--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/GuiUtils.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/GuiUtils.java
@@ -1,6 +1,7 @@
 package com.oheers.fish.gui;
 
 import com.oheers.fish.EvenMoreFish;
+import com.oheers.fish.FishUtils;
 import com.oheers.fish.config.GuiConfig;
 import com.oheers.fish.config.MainConfig;
 import com.oheers.fish.api.economy.Economy;
